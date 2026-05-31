bc683c705da49eec2f20d179f1b07be6446c9a45
rafay5k <rafay@tekrevol.com>
2026-05-31 23:04:32 +0500

    feat: Update review and waiter routes to remove authentication restrictions for GET requests

--- routes/waiterRoutes.js
+++ routes/waiterRoutes.js
@@ -19,10 +19,10 @@ const { protect, restrictTo } = require("../middlewares/authMiddleware");
 const { blockAdminWhenCriticalAlertPending } = require("../middlewares/adminAlertMiddleware");
 
 router.post("/", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), createWaiterValidationRules, validate, createWaiter);
-router.get("/", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), getWaiters);
+router.get("/", getWaiters);
 router.get("/name-id-list", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20, 40), getWaiterNameIdList);
 router.get("/search/by-name", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20, 40), getWaitersByName);
-router.get("/:id", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), getWaiterById);
+router.get("/:id", getWaiterById);
 router.put("/edit/:id", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), editWaiterValidationRules, validate, editWaiter);
 router.put("/:id", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), editWaiterValidationRules, validate, editWaiter);
 router.delete("/:id", protect, blockAdminWhenCriticalAlertPending, restrictTo(10, 20), deleteWaiterById);
